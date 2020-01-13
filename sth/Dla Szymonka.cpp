c180d8efb3997d1c8bb2f2cb3445b5162d4fff7e
Vorthan <59151946+Vorthan@users.noreply.github.com>
2020-01-13 18:44:51 +0100

    Menu

--- sth/Dla Szymonka.cpp
+++ sth/Dla Szymonka.cpp
@@ -2,24 +2,67 @@
 #include <iostream>
 int main()
 {
-    
+
 	droneList list;
-	drone d1;
-	drone d2;
-	drone d3;
+
 
 	
-	list.addEnd(&d1);
-	list.addEnd(&d2);
-	list.addEnd(&d3);
-	d1.IncreseCeiling(100);
-	d2.IncreseCeiling(200);
-	d3.IncreseCeiling(450);
-	d3.DecreaseCeiling(100);
-	list.printall();
-	list.increaseCellingAll(100);
+	int a, pomoc;
+	for (; 0 < 1;)
+	{
+		drone* tab;
+		drone* ptr;
+		std::cin >> a;
+		switch (a)
+		{
+		case 1:
+
+			std::cin >> pomoc;
+			tab = new drone[pomoc];
+			for (int i = 0; i < pomoc; i++)
+				list.addEnd(&tab[i]);
+			break;
+		case 2:
+			break;
+		case 3:
+			std::cin >> pomoc;
+			ptr = list.searchDrone(pomoc);
+			std::cin >> pomoc;
+			ptr->increseCeiling(pomoc);
+			break;
+		case 4:
+			std::cin >> pomoc;
+			ptr = list.searchDrone(pomoc);
+			std::cin >> pomoc;
+			ptr->decreaseCeiling(pomoc);
+			break;
+		case 5:
+			std::cin >> pomoc;
+			ptr = list.searchDrone(pomoc);
+			std::cin >> pomoc;
+			ptr->info();
+			break;
+		case 6:
+			std::cin >> pomoc;
+			list.decreaseCellingAll(pomoc);
+			break;
+			break;
+		case 7:
+			std::cin >> pomoc;
+			list.increseCellingAll(pomoc);
+			break;
+		case 8:
+			return 0;
+			break;
+		case 9:
+			list.printall();
+		default:
+			break;
 
 
+		}
+
+	}
 }
 
 
